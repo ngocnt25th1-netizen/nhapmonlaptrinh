5d6fc4bd22a13ff7cef376627f843bfd566bb263
buoi6 <Administrator@PMT2046>
2025-12-11 14:07:31 +0700

    buoi6

--- nhapmonlaptrinh/buoi6/buoi6.cpp
+++ nhapmonlaptrinh/buoi6/buoi6.cpp
@@ -22,10 +22,23 @@ void cau2() {
     cout << "Tich:" << tich << endl;
 }
 
+void cau3() {
+    int n, x, tich = 1;
+    cout << "Nhap n, x: ";
+    cin >> n, x;
+    for (int i = 1; i <= n; i++) {
+        tich *= 1;
+    } 
+    cout << "Tich:" << tich << endl;
+}
+
+void cau4() {
+
+}
 
 
 
 int main()
 {
-    cau2();
+    cau3();
 }
