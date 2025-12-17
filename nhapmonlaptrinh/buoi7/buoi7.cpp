60b3968fb67f890500875259100a2075a65e3080
buoi7 <Administrator@PMT2046>
2025-12-17 15:08:06 +0700

    buoi7

--- nhapmonlaptrinh/buoi7/buoi7.cpp
+++ nhapmonlaptrinh/buoi7/buoi7.cpp
@@ -1,14 +1,14 @@
 
+#include "BaiTap6.h"
 
-#include <iostream>
-#include "SoNguyen.h"
-using namespace std;
 int main()
 {
-	int a = 25, b = 60;
-	int uocchunglonnhat = UCLN(a, b);
-	cout << "Uoc chung lon nhat: " << uocchunglonnhat << endl;
-	
+	cau11();
 }
+
+
+
+
+
 
 	
